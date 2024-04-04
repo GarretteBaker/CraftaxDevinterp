d43ee4b706625669094cea3a09b8784856b8b621
garrettebaker <garrettebaker@outlook.com>
2024-04-04 02:31:17 +0000

    Update intermediate directory path in PPO training loop

--- craftax/ppo.py
+++ craftax/ppo.py
@@ -658,9 +658,9 @@ def make_train(config):
             for update_no in range(int(config["NUM_UPDATES"])):
                 runner_state, single_metric_obs = _update_step_jit(runner_state, metric)
                 trajectory = single_metric_obs.traj
-                os.makedirs(f"/workspace/CraftaxDevinterp/intermediate", exist_ok=True)
+                os.makedirs(f"/workspace/CraftaxDevinterp/intermediate/{update_no}", exist_ok=True)
                 train_state = jax.tree_map(lambda x: x[0], trajectory)
-                path = ocp.test_utils.erase_and_create_empty("/workspace/CraftaxDevinterp/intermediate")
+                path = ocp.test_utils.erase_and_create_empty(f"/workspace/CraftaxDevinterp/intermediate/{update_no}")
                 checkpoint_name = f"model_{update_no}"
                 checkpointer = ocp.StandardCheckpointer()
                 checkpointer.save(path / checkpoint_name, train_state)
