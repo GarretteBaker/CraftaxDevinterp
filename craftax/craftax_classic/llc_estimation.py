6c555c919458a631f7ddd4951cac9e497faf92e9
garrettebaker <garrettebaker@outlook.com>
2024-04-26 20:56:50 +0000

    classic llc estimation (with some mods to sgld utils)

--- craftax/craftax_classic/llc_estimation.py
+++ craftax/craftax_classic/llc_estimation.py
@@ -42,7 +42,7 @@ rng = jax.random.PRNGKey(seed)
 rng, rng_train, rng_sgld = jax.random.split(rng, num=3)
 
 #%%
-def generate_trajectory(network_params, rng, num_envs=1000, num_steps=495):
+def generate_trajectory(network_params, rng, num_envs=1, num_steps=495):
     env = CraftaxClassicSymbolicEnv()
     env = AutoResetEnvWrapper(env)
     env = BatchEnvWrapper(env, num_envs)
@@ -102,15 +102,21 @@ def generate_trajectory(network_params, rng, num_envs=1000, num_steps=495):
     return traj_batch.obs, traj_batch.action_logits
 
 # jit_gen_traj_100 = jax.jit(lambda params, rng: generate_trajectory(params, rng, num_steps=128))
-jit_gen_traj_495 = jax.jit(lambda params, rng: generate_trajectory(params, rng, num_steps=100))
+jit_gen_traj_495 = jax.jit(lambda params, rng: generate_trajectory(params, rng, num_steps=10000))
 #%%
+network = ActorCritic(17, layer_size)
+rng, _rng = jax.random.split(rng)
+init_x = jnp.zeros((1, 1345))
+network_params = network.init(_rng, init_x)
+abstract_params = jax.tree_util.tree_map(
+    ocp.utils.to_shape_dtype_struct, network_params)
 model_no = -1
 checkpoint_directory = f"/workspace/CraftaxDevinterp/intermediate"
 model_list = os.listdir(checkpoint_directory)
 model_file = f"{checkpoint_directory}/{model_list[model_no]}"
 folder_list = os.listdir(model_file)
 checkpointer = ocp.StandardCheckpointer()
-network_params = checkpointer.restore(f"{model_file}/{folder_list[0]}")
+network_params = checkpointer.restore(f"{model_file}/{folder_list[0]}", args=ocp.args.StandardRestore(abstract_params))
 # expert_obses_train, expert_logitses_train = jit_gen_traj_100(network_params, rng_train)
 expert_obses, expert_logitses = jit_gen_traj_495(network_params, rng_sgld)
 expert_obses = expert_obses.reshape(expert_obses.shape[0] * expert_obses.shape[1], expert_obses.shape[2])
@@ -124,15 +130,15 @@ def mse_loss(param, model, inputs, targets):
 env = CraftaxClassicSymbolicEnv()
 env_params = env.default_params
 env = LogWrapper(env)
-env = OptimisticResetVecEnvWrapper(
-    env,
-    num_envs=1,
-    reset_ratio=min(16, 1),
-)
-network = ActorCritic(env.action_space(env_params).n, layer_size)
+# env = OptimisticResetVecEnvWrapper(
+#     env,
+#     num_envs=1,
+#     reset_ratio=min(16, 1),
+# )
 loss_fn = jax.jit(
     lambda param, inputs, targets: mse_loss(param, network, inputs, targets)
 )
+print(env.observation_space(env_params).shape)
 
 # model_no = 100
 # checkpoint_directory = f"/workspace/CraftaxDevinterp/intermediate"
@@ -238,100 +244,112 @@ loss_fn = jax.jit(
 # trained_params = trained_state.params
 
 #%%
-eps_lower_pow = -4
+# eps_lower_pow = -6
+# # eps_upper_pow = -11
+# eps_upper_pow = -9
+# num_eps = int(abs(eps_lower_pow - eps_upper_pow)) + 1
+
+# # gam_lower_pow = -3
+# # gam_upper_pow = 4
+# gam_lower_pow = 0
+# gam_upper_pow = 3
+# num_gam = int(gam_upper_pow - gam_lower_pow) + 1
+
+# os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/debug", exist_ok=True)
+# fig, axs = plt.subplots(num_eps, num_gam, figsize=(16*num_eps, 16*num_gam))
+# for i, epsilon in tqdm(enumerate(np.logspace(eps_lower_pow, eps_upper_pow, num=num_eps, base=10)), desc="Epsilon", total=num_eps):
+#     for j, gamma in tqdm(enumerate(np.logspace(gam_lower_pow, gam_upper_pow, num=num_gam, base=10)), desc="Gamma", total=num_gam):
+#         sgld_config = SGLDConfig(
+#             epsilon = epsilon, 
+#             gamma = gamma, 
+#             num_steps = 1000, 
+#             num_chains = 1,
+#             batch_size = 64)
+
+#         num_training_data = len(expert_obses)
+#         itemp = 1/np.log(num_training_data)
+
+#         loss_trace, _, acceptance_probs = run_sgld(
+#             rng_sgld, 
+#             loss_fn, 
+#             sgld_config, 
+#             network_params, 
+#             expert_obses, 
+#             expert_logitses, 
+#             itemp = itemp, 
+#             trace_batch_loss = True, 
+#             compute_distance = False, 
+#             verbose = False
+#         )
+
+#         init_loss = loss_fn(network_params, expert_obses, expert_logitses)
+#         lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
+#         axs[i, j].plot(loss_trace)
+#         axs[i, j].axhline(y=init_loss, linestyle=':')
+#         axs[i, j].set_title(f"epsilon {epsilon}, gamma {gamma}, lambda {lambdahat}, mala {np.mean(np.array(acceptance_probs)[:, 1])}")
+# plt.tight_layout()
+# plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/debug/calibration.png")
+
+
+#%%
+model_no = 100
+checkpoint_directory = f"/workspace/CraftaxDevinterp/intermediate"
+model_list = os.listdir(checkpoint_directory)
+model_file = f"{checkpoint_directory}/{model_list[model_no]}"
+folder_list = os.listdir(model_file)
+checkpointer = ocp.StandardCheckpointer()
+network_params = checkpointer.restore(f"{model_file}/{folder_list[0]}", args=ocp.args.StandardRestore(abstract_params))
+
+beta_lower_pow = 0
+beta_upper_pow = -7
+beta_upper_pow = -4
+num_beta = int(abs(beta_upper_pow - beta_lower_pow)) + 1
+
+eps_lower_pow = -5
 eps_upper_pow = -11
 num_eps = int(abs(eps_lower_pow - eps_upper_pow)) + 1
 
-gam_lower_pow = -3
+gam_lower_pow = 0
 gam_upper_pow = 4
 num_gam = int(gam_upper_pow - gam_lower_pow) + 1
 
 os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/debug", exist_ok=True)
-fig, axs = plt.subplots(num_eps, num_gam, figsize=(16*num_eps, 16*num_gam))
-for i, epsilon in tqdm(enumerate(np.logspace(eps_lower_pow, eps_upper_pow, num=num_eps, base=10)), desc="Epsilon", total=num_eps):
-    for j, gamma in tqdm(enumerate(np.logspace(gam_lower_pow, gam_upper_pow, num=num_gam, base=10)), desc="Gamma", total=num_gam):
-        sgld_config = SGLDConfig(
-            epsilon = epsilon, 
-            gamma = gamma, 
-            num_steps = 1000, 
-            num_chains = 1,
-            batch_size = 64)
+for k, epsilon in tqdm(enumerate(np.logspace(eps_lower_pow, eps_upper_pow, num=num_eps, base=10)), desc="Epsilon", total=num_eps):
+    fig, axs = plt.subplots(num_beta, num_gam, figsize=(16*num_beta, 16*num_gam))
+    for i, beta in tqdm(enumerate(np.logspace(beta_lower_pow, beta_upper_pow, num=num_beta, base=10)), desc="Beta", total=num_beta):
+        for j, gamma in tqdm(enumerate(np.logspace(gam_lower_pow, gam_upper_pow, num=num_gam, base=10)), desc="Gamma", total=num_gam):
+            sgld_config = SGLDConfig(
+                epsilon = epsilon, 
+                gamma = gamma, 
+                num_steps = 1000, 
+                num_chains = 1,
+                batch_size = 64)
 
-        num_training_data = len(expert_obses)
-        itemp = 1/np.log(num_training_data)
+            num_training_data = len(expert_obses)
+            itemp = beta
 
-        loss_trace, _, acceptance_probs = run_sgld(
-            rng_sgld, 
-            loss_fn, 
-            sgld_config, 
-            network_params, 
-            expert_obses, 
-            expert_logitses, 
-            itemp = itemp, 
-            trace_batch_loss = True, 
-            compute_distance = False, 
-            verbose = False
-        )
+            loss_trace, _, acceptance_probs = run_sgld(
+                rng_sgld, 
+                loss_fn, 
+                sgld_config, 
+                network_params, 
+                expert_obses, 
+                expert_logitses, 
+                itemp = itemp, 
+                trace_batch_loss = True, 
+                compute_distance = False, 
+                verbose = False
+            )
 
-        init_loss = loss_fn(network_params, expert_obses, expert_logitses)
-        lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
-        axs[i, j].plot(loss_trace)
-        axs[i, j].axhline(y=init_loss, linestyle=':')
-        axs[i, j].set_title(f"epsilon {epsilon}, gamma {gamma}, lambda {lambdahat}, mala {np.mean(np.array(acceptance_probs)[:, 1])}")
-plt.tight_layout()
-plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/debug/calibration.png")
-
-
-# #%%
-# beta_lower_pow = 0
-# beta_upper_pow = -2
-# num_beta = int(abs(beta_upper_pow - beta_lower_pow)) + 1
-
-# eps_lower_pow = -4
-# eps_upper_pow = -11
-# num_eps = int(abs(eps_lower_pow - eps_upper_pow)) + 1
-
-# gam_lower_pow = -3
-# gam_upper_pow = 4
-# num_gam = int(gam_upper_pow - gam_lower_pow) + 1
-
-# os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/debug", exist_ok=True)
-# fig, axs = plt.subplots(num_beta, num_gam, figsize=(16*num_beta, 16*num_gam))
-# for k, epsilon in tqdm(enumerate(np.logspace(eps_lower_pow, eps_upper_pow, num=num_eps, base=10)), desc="Epsilon", total=num_eps):
-#     for i, beta in tqdm(enumerate(np.logspace(beta_lower_pow, beta_upper_pow, num=num_beta, base=10)), desc="Beta", total=num_beta):
-#         for j, gamma in tqdm(enumerate(np.logspace(gam_lower_pow, gam_upper_pow, num=num_gam, base=10)), desc="Gamma", total=num_gam):
-#             sgld_config = SGLDConfig(
-#                 epsilon = epsilon, 
-#                 gamma = gamma, 
-#                 num_steps = 1000, 
-#                 num_chains = 1,
-#                 batch_size = 64)
-
-#             num_training_data = len(expert_obses)
-#             itemp = beta
-
-#             loss_trace, _, acceptance_probs = run_sgld(
-#                 rng_sgld, 
-#                 loss_fn, 
-#                 sgld_config, 
-#                 network_params, 
-#                 expert_obses, 
-#                 expert_logitses, 
-#                 itemp = itemp, 
-#                 trace_batch_loss = True, 
-#                 compute_distance = False, 
-#                 verbose = False
-#             )
-
-#             init_loss = loss_fn(network_params, expert_obses, expert_logitses)
-#             lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
-#             axs[i, j].plot(loss_trace)
-#             axs[i, j].axhline(y=init_loss, linestyle=':')
-#             axs[i, j].set_title(f"epsilon {epsilon}, beta {beta}, gamma {gamma}, lambda {lambdahat}, mala {np.mean(np.array(acceptance_probs)[:, 1])}")
-#     plt.tight_layout()
-#     plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/debug/calibration_eps_{epsilon}.png")
-#     plt.close()
-# #%%
+            init_loss = loss_fn(network_params, expert_obses, expert_logitses)
+            lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
+            axs[i, j].plot(loss_trace)
+            axs[i, j].axhline(y=init_loss, linestyle=':')
+            axs[i, j].set_title(f"epsilon {epsilon}, beta {beta}, gamma {gamma}, lambda {lambdahat}, mala {np.mean(np.array(acceptance_probs)[:, 1])}")
+    plt.tight_layout()
+    plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/debug/calibration_eps_{epsilon}.png")
+    plt.close()
+#%%
 # # import time
 # # t0 = time.time()
 # # sgld_config = SGLDConfig(
@@ -392,80 +410,82 @@ plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/debug/calibration.png")
 # #     lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
 # #     plt.plot(loss_trace)
 # # plt.show()
-# #%%
+#%%
 
-# sgld_config = SGLDConfig(
-#     epsilon = 0.0001, 
-#     gamma = 10, 
-#     num_steps = 3000, 
-#     num_chains = 1,
-#     batch_size = 64)
+sgld_config = SGLDConfig(
+    epsilon = 0.0001, 
+    gamma = 10, 
+    num_steps = 10, 
+    num_chains = 1,
+    batch_size = 64)
 
-# num_models = 1525
-# os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_curves", exist_ok = True)
-# os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats", exist_ok=True)
-# os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_data", exist_ok=True)
-# itemp = 0.01
-# num_training_data = len(expert_obses)
+num_models = len(os.listdir("/workspace/CraftaxDevinterp/intermediate"))
+os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_curves", exist_ok = True)
+os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats", exist_ok=True)
+os.makedirs("/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_data", exist_ok=True)
+itemp = 0.01
+num_training_data = len(expert_obses)
 
-# sgld_params = get_sgld_params(
-#     rng_sgld, 
-#     loss_fn, 
-#     sgld_config, 
-#     network_params, 
-#     expert_obses, 
-#     expert_logitses, 
-#     itemp = itemp, 
-#     trace_batch_loss = True, 
-#     compute_distance = False, 
-#     verbose = False
-# )
-# jitted_sgld_run = jax.jit(lambda network_params: sgld_run(
-#     sgld_params[0], 
-#     sgld_params[1], 
-#     sgld_params[2], 
-#     sgld_params[3], 
-#     sgld_params[4], 
-#     sgld_params[5], 
-#     sgld_params[6], 
-#     sgld_params[7], 
-#     network_params
-# ))
-# for model_no in tqdm(range(0, num_models, 1)):
-#     checkpoint_directory = f"/workspace/CraftaxDevinterp/intermediate/{model_no}"
-#     checkpointer = ocp.StandardCheckpointer()
-#     folder_list = os.listdir(checkpoint_directory)
-#     network_params = checkpointer.restore(f"{checkpoint_directory}/{folder_list[0]}")
+sgld_params = get_sgld_params(
+    rng_sgld, 
+    loss_fn, 
+    sgld_config, 
+    network_params, 
+    expert_obses, 
+    expert_logitses, 
+    itemp = itemp, 
+    trace_batch_loss = True, 
+    compute_distance = False, 
+    verbose = False
+)
+jitted_sgld_run = jax.jit(lambda network_params: sgld_run(
+    sgld_params[0], 
+    sgld_params[1], 
+    sgld_params[2], 
+    sgld_params[3], 
+    sgld_params[4], 
+    sgld_params[5], 
+    sgld_params[6], 
+    sgld_params[7], 
+    network_params
+))
+checkpoint_list = os.listdir(f"/workspace/CraftaxDevinterp/intermediate")
+for model_no in tqdm(range(0, num_models, 1)):
+    checkpoint_directory = f"/workspace/CraftaxDevinterp/intermediate/{checkpoint_list[model_no]}"
+    checkpointer = ocp.StandardCheckpointer()
+    folder_list = os.listdir(checkpoint_directory)
+    network_params = checkpointer.restore(f"{checkpoint_directory}/{folder_list[0]}", args=ocp.args.StandardRestore(abstract_params))
 
-#     loss_trace = jitted_sgld_run(network_params)
+    loss_trace = jitted_sgld_run(network_params)
+    print(loss_trace.shape)
 
-#     init_loss = loss_fn(network_params, expert_obses, expert_logitses)
-#     lambdahat = float(np.mean(loss_trace[2000:]) - init_loss) * num_training_data * itemp
-#     with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats/{model_no}.pkl", "wb") as f:
-#         pickle.dump(lambdahat, f)
-#     with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_data/{model_no}.pkl", "wb") as f:
-#         pickle.dump((loss_trace, init_loss), f)
-#     plt.plot(loss_trace)
-#     plt.axhline(y=init_loss, linestyle=':')
-#     plt.title(f"lambda {lambdahat}")
-#     plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_curves/{model_no}.png")
-#     plt.close()
+    init_loss = loss_fn(network_params, expert_obses, expert_logitses)
+    lambdahat = float(np.mean(loss_trace) - init_loss) * num_training_data * itemp
+    with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats/{model_no}.pkl", "wb") as f:
+        pickle.dump(lambdahat, f)
+    with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_data/{model_no}.pkl", "wb") as f:
+        pickle.dump((loss_trace, init_loss), f)
+    plt.plot(loss_trace)
+    plt.axhline(y=init_loss, linestyle=':')
+    plt.title(f"lambda {lambdahat}")
+    plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/trace_curves/{model_no}.png")
+    plt.close()
 
-# # %%
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pickle
-# from tqdm import tqdm
-# num_models = 1525
-# num_training_data = len(expert_obses)
-# print("loading lambdahats")
-# lambdahats = list()
-# for modelno in tqdm(range(0, num_models, 1)):
-#     with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats/{modelno}.pkl", "rb") as f:
-#         lambdahat = pickle.load(f)
-#     lambdahats.append(lambdahat)
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from tqdm import tqdm
+num_models = 350
+num_training_data = len(expert_obses)
+print("loading lambdahats")
+lambdahats = list()
+for modelno in tqdm(range(0, num_models, 1)):
+    with open(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats/{modelno}.pkl", "rb") as f:
+        lambdahat = pickle.load(f)
+    lambdahats.append(lambdahat)
 
-# plt.plot(lambdahats)
-# plt.title(f"Lambdahats")
-# plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats.png")
-# # %%
+plt.plot(lambdahats)
+plt.title(f"Lambdahats")
+plt.savefig(f"/workspace/CraftaxDevinterp/llc_estimation/water_tree_restricted/lambdahats.png")
+# %%
