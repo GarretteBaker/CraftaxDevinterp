11f7d0dad6433995bf964f979ef99987ad9efed0
garrettebaker <garrettebaker@outlook.com>
2024-07-25 18:19:17 +0000

    turned model trajectory into a standalone file

---
+++ craftax/craftax_classic/make_model_trajectory.py
@@ -0,0 +1,82 @@
+import jax
+import jax.numpy as jnp
+from typing import NamedTuple
+from craftax.environment_base.wrappers import (
+    AutoResetEnvWrapper,
+    BatchEnvWrapper,
+)
+from craftax.models.actor_critic import (
+    ActorCritic,
+)
+from craftax.craftax_classic.envs.craftax_symbolic_env import CraftaxClassicSymbolicEnv
+
+
+class Tracker(NamedTuple):
+    block_placements: jnp.ndarray
+    block_mining: jnp.ndarray
+    player_location: jnp.ndarray
+    player_movement: jnp.ndarray
+    # revealed_blocks: jnp.ndarray
+    doings: jnp.ndarray
+    mob_kills: jnp.ndarray
+    mob_attacks: jnp.ndarray
+    time: jnp.ndarray
+
+
+def generate_trajectory(network_params, rng, num_envs, num_steps):
+    env = CraftaxClassicSymbolicEnv()
+    env = AutoResetEnvWrapper(env)
+    env = BatchEnvWrapper(env, num_envs)
+    env_params = env.default_params
+    network = ActorCritic(env.action_space(env_params).n, 512, activation='relu')
+
+    class Transition(NamedTuple):
+        tracking: jnp.ndarray
+        done: jnp.ndarray
+
+    # COLLECT TRAJECTORIES
+    def _env_step(runner_state, unused):
+        (
+            past_state,
+            last_obs,
+            rng,
+        ) = runner_state
+
+        # SELECT ACTION
+        rng, _rng = jax.random.split(rng)
+        pi, _ = network.apply(network_params, last_obs)
+        action = pi.sample(seed=_rng)
+
+        # STEP ENV
+        rng, _rng = jax.random.split(rng)
+        obsv, env_state, _, done, _ = env.step(
+            _rng, past_state, action, env_params
+        )
+
+        tracker = past_state
+
+        transition = Transition(
+            tracking = tracker,
+            done = done
+        )
+        runner_state = (
+            env_state,
+            obsv,
+            rng,
+        )
+        return runner_state, transition
+    rng, _rng = jax.random.split(rng)
+    obsv, env_state = env.reset(_rng, env_params)
+
+    rng, _rng = jax.random.split(rng)
+    runner_state = (
+        env_state,
+        obsv,
+        _rng,
+    )
+
+    runner_state, traj_batch = jax.lax.scan(
+        _env_step, runner_state, None, num_steps
+    )
+    return traj_batch.tracking, traj_batch.done
+jit_gen_traj = jax.jit(generate_trajectory, static_argnames=["num_envs", "num_steps"])
